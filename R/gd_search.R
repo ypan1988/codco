4734e1849d822158ba833169097db6c9dbcddac9
samuel-watson <samuel.i.watsom@gmail.com>
2022-01-10 19:25:47 +0000

    Added grad_robust_step() and reorder_obs()

--- R/gd_search.R
+++ R/gd_search.R
@@ -19,7 +19,7 @@ choose_swap <- function(idx_in,A,sig,u){
   return(list(val,idx_in,newA))
 }
 
-choose_swap_robust <- function(idx_in,A_list,sig_list,u_list,weights){
+choose_swap_robust <- function(idx_in,A_list,sig_list,u_list,weights,varval){
   idx_out <- 1:nrow(sig_list[[1]])
   idx_out <- idx_out[!idx_out%in%idx_in]
 
@@ -31,6 +31,7 @@ choose_swap_robust <- function(idx_in,A_list,sig_list,u_list,weights){
       remove_one(A_list[[idx]],i-1,u_list[[idx]][idx_in]))
   }
   val_out <- as.numeric(val_out_mat %*% weights)
+  
 
   rm1A <- list()
   for(idx in 1:length(A_list)){
@@ -38,7 +39,7 @@ choose_swap_robust <- function(idx_in,A_list,sig_list,u_list,weights){
   }
 
   idx_in <- idx_in[-which.max(val_out)]
-
+  
   for(idx in 1:length(A_list)){
     val_in_mat[,idx] <- sapply(idx_out,function(i)add_one(rm1A[[idx]],
                                                           sig_list[[idx]][i,i],
@@ -46,6 +47,7 @@ choose_swap_robust <- function(idx_in,A_list,sig_list,u_list,weights){
                                                           u_list[[idx]][c(idx_in,i)]))
   }
   val_in <- as.numeric(val_in_mat %*% weights)
+  #print(varval - val_in)
   swap_idx <- idx_out[which.max(val_in)]
 
   newA <- list()
@@ -54,6 +56,7 @@ choose_swap_robust <- function(idx_in,A_list,sig_list,u_list,weights){
                                sig_list[[idx]][idx_in,swap_idx])
   }
   idx_in <- c(idx_in,swap_idx)
+
   val <- val_in[which.max(val_in)]
   return(list(val,idx_in,newA))
 }
@@ -168,7 +171,7 @@ grad_robust <- function(idx_in,
   # the objective function here is now c^T M^-1 c - i've implemented c_obj_func in gd_search.cpp
   new_val_vec <- matrix(sapply(1:length(A_list),function(i)c_obj_fun(M_list[[i]], C_list[[i]])),nrow=1)
   new_val <- as.numeric(new_val_vec %*% w)
-  
+
   diff <- -1
   i <- 0
   # we now need diff to be negative
@@ -182,8 +185,8 @@ grad_robust <- function(idx_in,
     # }
     
     val <- new_val
-    i <- i + 1
-    out <- choose_swap_robust(idx_in,A_list,sig_list,u_list, w)
+    i <- i + 1 
+    out <- choose_swap_robust(idx_in,A_list,sig_list,u_list, w,val)
     # new_val <- out[[1]]
     
     #we have to now recalculate all the lists of matrices for the new design proposed by the swap
@@ -280,6 +283,286 @@ grad_robust <- function(idx_in,
   }
   
   return(idx_in)
+}
+
+#Updated version of algorithm below, plus reorder_obs() function below it
+grad_robust_step <- function(idx_in,
+                        C_list,
+                        X_list,
+                        sig_list,
+                        w=NULL,
+                        trace = TRUE,
+                        rm_cols = NULL){
+  
+  if(is.null(w))w <- rep(1/length(sig_list),length(sig_list))
+  if(sum(w)!=1)w <- w/sum(w)
+  if(!is(w,"matrix"))w <- matrix(w,ncol=1)
+  if(!all(unlist(lapply(C_list,function(x)is(x,"matrix")))))stop("All C_list must be matrices")
+  if(!all(unlist(lapply(sig_list,function(x)is(x,"matrix")))))stop("All sig_list must be matrices")
+  if(!all(unlist(lapply(X_list,function(x)is(x,"matrix")))))stop("All X_list must be matrices")
+  if((length(C_list)!=length(X_list))|length(X_list)!=length(sig_list))stop("Lists must be same length")
+  
+  #define sample size
+  n <- length(idx_in)
+  
+  # added this function to give the user the option to remove columns from particular
+  # designs quickly if the algorithm previously stopped and said to remove
+  if(!is.null(rm_cols))
+  {
+    if(!is(rm_cols,"list"))stop("rm_cols should be a list")
+    idx_original <- list()
+    zero_idx <- c()
+    idx_original <- 1:nrow(X_list[[1]])
+    
+    # find all the entries with non-zero values of the given columns in each design
+    for(i in 1:length(rm_cols))
+    {
+      if(!is.null(rm_cols[[i]])){
+        for(j in 1:length(rm_cols[[i]]))
+        {
+          zero_idx <- c(zero_idx,which(X_list[[i]][,rm_cols[[i]][j]]!=0))
+        }
+      }
+    }
+    zero_idx <- sort(unique(zero_idx))
+    idx_original <- idx_original[-zero_idx]
+    idx_in <- match(idx_in,idx_original)
+    
+    if(trace)message(paste0("removing ",length(zero_idx)," observations"))
+    
+    #update the matrices
+    for(i in 1:length(rm_cols))
+    {
+      X_list[[i]] <- X_list[[i]][-zero_idx,-rm_cols[[i]]]
+      C_list[[i]] <- matrix(C_list[[i]][-rm_cols[[i]]],ncol=1)
+      sig_list[[i]] <- sig_list[[i]][-zero_idx,-zero_idx]
+      
+    }
+    
+    if(any(is.na(idx_in)))
+    {
+      if(trace)message("generating new random starting point")
+      idx_in <- sample(1:nrow(X_list[[1]]),n,replace=FALSE)
+    }
+  }
+  
+  #MAIN BODY OF THE FUNCTION
+  
+  # we need to calculate the M matrices for all the designs and store them
+  # M is calculated for idx_in design rather than full design
+  A_list <- list()
+  A_list_sub <- list()
+  u_list <- list()
+  M_list <- list()
+  rm1A <- list()
+  for(i in 1:length(sig_list))
+  {
+    A_list[[i]] <- solve(sig_list[[i]][idx_in,idx_in])
+    M_list[[i]] <- gen_m(X_list[[i]][idx_in,],A_list[[i]])
+    u_list[[i]] <- (t(C_list[[i]]) %*% solve(M_list[[i]])) %*% t(X_list[[i]])
+  }
+  
+  # the objective function here is now c^T M^-1 c - i've implemented c_obj_func in gd_search.cpp
+  new_val_vec <- matrix(sapply(1:length(A_list),function(i)c_obj_fun(M_list[[i]], C_list[[i]])),nrow=1)
+  new_val <- as.numeric(new_val_vec %*% w)
+  
+  diff <- -1
+  i <- 0
+  
+  #1. order the list of observations to include and exclude
+  # idx_in are the observations in the design and idx_out are the observations not in the design
+  idx_list <- reorder_obs(idx_in,
+                          A_list,
+                          sig_list,
+                          u_list,
+                          w)
+  idx_in <- idx_list[[1]]
+  idx_out <- idx_list[[2]]
+  A_list <- idx_list[[3]]
+  
+  while(diff < 0)
+  {
+    val <- new_val
+    i <- i + 1 
+    
+    #2. make swap at top of list
+    
+    #grad <- c()
+    for(idx in 1:length(A_list)){
+      rm1A[[idx]] <- remove_one_mat(A_list[[idx]],0)
+      A_list_sub[[idx]] <- add_one_mat(rm1A[[idx]],
+                                   sig_list[[idx]][idx_out[1],idx_out[1]],
+                                   sig_list[[idx]][idx_in[2:n],idx_out[1]])
+      M_list[[idx]] <- gen_m(X_list[[idx]][c(idx_in[2:n],idx_out[1]),],A_list_sub[[idx]])
+      
+      # check positive semi-definite before proceeding
+      if(!check_psd(M_list[[idx]]))stop(paste0("M not positive semi-definite. Column ",which(colSums(M_list[[idx]])==0),
+                                             " of design ",idx," is not part of an optimal design."))
+    }
+
+    #calculate values for the new design - this is changed to new objective function
+    new_val_vec <- matrix(sapply(1:length(A_list),function(j)c_obj_fun(M_list[[j]], C_list[[j]])),nrow=1)
+    new_val <- as.numeric(new_val_vec %*% w)
+    #print(c(idx_in[1],idx_out[1],val,new_val))
+    
+    diff <- new_val - val
+    
+    if (trace) {
+      cat("\nIter: ",i)
+      cat(" ",diff)
+    }
+    
+    if(diff<0){
+      swap_idx <- idx_in[1]
+      idx_in <- c(idx_in[2:n],idx_out[1])
+      idx_out <- c(idx_out[2:n],swap_idx)
+      A_list <- A_list_sub
+      for(idx in 1:length(A_list))
+      {
+        u_list[[idx]] <- (t(C_list[[idx]]) %*% solve(M_list[[idx]])) %*% t(X_list[[idx]])
+      }
+    } else {
+      #cat("\nReordering")
+      i <- i + 1 
+      idx_list <- reorder_obs(idx_in,
+                              A_list,
+                              sig_list,
+                              u_list,
+                              w)
+      idx_in <- idx_list[[1]]
+      idx_out <- idx_list[[2]]
+      A_list <- idx_list[[3]]
+   
+      for(idx in 1:length(A_list))
+      {
+        rm1A[[idx]] <- remove_one_mat(A_list[[idx]],0)
+        A_list_sub[[idx]] <- add_one_mat(rm1A[[idx]],
+                                     sig_list[[idx]][idx_out[1],idx_out[1]],
+                                     sig_list[[idx]][idx_in[2:n],idx_out[1]])
+        
+        M_list[[idx]] <- gen_m(X_list[[idx]][c(idx_in[2:n],idx_out[1]),],A_list_sub[[idx]])
+        
+        
+        # check positive semi-definite before proceeding
+        if(!check_psd(M_list[[idx]]))stop(paste0("M not positive semi-definite. Column ",which(colSums(M_list[[idx]])==0),
+                                               " of design ",idx," is not part of an optimal design."))
+      }
+      
+      
+      #calculate values for the new design - this is changed to new objective function
+      new_val_vec <- matrix(sapply(1:length(A_list),function(j)c_obj_fun(M_list[[j]], C_list[[j]])),nrow=1)
+      new_val <- as.numeric(new_val_vec %*% w)
+      
+      diff <- new_val - val
+      
+      # we are now looking for the smallest value rather than largest so diff<0
+      if (trace) {
+        cat("\nIter (reorder): ",i)
+        cat(" ",diff)
+      }
+      
+      if(diff<0){
+        swap_idx <- idx_in[1]
+        idx_in <- c(idx_in[2:n],idx_out[1])
+        idx_out <- c(idx_out[2:n],swap_idx)
+        A_list <- A_list_sub
+        for(idx in 1:length(A_list))
+        {
+          u_list[[idx]] <- (t(C_list[[idx]]) %*% solve(M_list[[idx]])) %*% t(X_list[[idx]])
+        }
+      }
+      
+    }
+    
+  }
+  
+  # as a check, see if the next swap would mean that M is not positive semi-definite, in which
+  # case the algorithm has terminated early and the solution is not in this design
+  # this code is copied from the choose_swap_robust function, so could be streamlined into its own function perhaps
+  
+  idx_test <- c(idx_in[2:n],idx_out[1])
+  val_out_mat <- matrix(NA,nrow=length(idx_test),ncol=length(A_list))
+  for(idx in 1:length(A_list)){
+    val_out_mat[,idx] <- sapply(1:length(idx_test),function(i)
+      remove_one(A_list[[idx]],i-1,u_list[[idx]][idx_test]))
+  }
+  val_out <- as.numeric(val_out_mat %*% w)
+  idx_test <- idx_test[-which.max(val_out)]
+  rm1A <- list()
+  for(idx in 1:length(A_list)){
+    rm1A[[idx]] <- remove_one_mat(A_list[[idx]],which.max(val_out)-1)
+  }
+  for(j in 1:length(sig_list))
+  {
+    M_list[[j]] <- gen_m(X_list[[j]][idx_test,],rm1A[[j]])
+    
+    if(!check_psd(M_list[[j]]))stop(paste0("M not positive semi-definite. Column ",which(colSums(M_list[[j]])==0),
+                                           " of design ",j," is not part of an optimal design."))
+  }
+  
+
+  ## if columns were removed then return the index to the indexing of the original X matrix
+  if(!is.null(rm_cols))
+  {
+    idx_in <- idx_original[idx_in]
+  }
+  
+  #return variance
+  if(trace){
+    var_vals <- c()
+    for(i in 1:length(X_list))
+    {
+      var_vals[i] <- new_val_vec[i] #tryCatch(c_obj_fun(C_list[[i]],X_list[[i]][sort(idx_in),],sig_list[[i]][sort(idx_in),sort(idx_in)]),error=function(i){NA})
+    }
+    cat("\nVariance for individual model(s):\n")
+    print(var_vals)
+    if(length(A_list)>1){
+      cat("\n Weighted average variance:\n")
+      print(sum(var_vals*c(w)))
+    }
+  }
+  
+  return(idx_in)
+}
+
+#function to reorder the observations
+reorder_obs <- function(idx_in,
+                        A_list,
+                        sig_list,
+                        u_list,
+                        weights){
+  
+  idx_out <- 1:nrow(sig_list[[1]])
+  idx_out <- idx_out[!idx_out%in%idx_in]
+  n <- length(idx_in)
+  
+  val_out_mat <- matrix(NA,nrow=length(idx_in),ncol=length(A_list))
+  val_in_mat <- matrix(NA,nrow=length(idx_out),ncol=length(A_list))
+  
+  for(idx in 1:length(A_list)){
+    val_out_mat[,idx] <- sapply(1:length(idx_in),function(i)
+      remove_one(A_list[[idx]],i-1,u_list[[idx]][idx_in]))
+  }
+  val_out <- as.numeric(val_out_mat %*% weights)
+  #print(head(data.frame(val=sort(val_out,decreasing = TRUE),idx=idx_in[order(val_out,decreasing = TRUE)])))
+  idx_in <- idx_in[order(val_out, decreasing = TRUE)]
+  
+  #rm1A <- list()
+  
+  for(idx in 1:length(A_list)){
+    A_list[[idx]] <- A_list[[idx]][order(val_out, decreasing = TRUE),order(val_out, decreasing = TRUE)]
+    #rm1A[[idx]] <- remove_one_mat(A_list[[idx]],0)
+    val_in_mat[,idx] <- sapply(idx_out,function(i)add_one(A_list[[idx]],
+                                                          sig_list[[idx]][i,i],
+                                                          sig_list[[idx]][idx_in,i],
+                                                          u_list[[idx]][c(idx_in,i)]))
+  }
+  
+  val_in <- as.numeric(val_in_mat %*% weights)
+  #print(head(data.frame(val=sort(val_in,decreasing = TRUE),idx=idx_out[order(val_in,decreasing = TRUE)])))
+  idx_out <- idx_out[order(val_in,decreasing = TRUE)]
+  
+  return(list(idx_in,idx_out,A_list))
 }
 
 optim_fun <- function(C,X,S){
